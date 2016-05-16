c7cbaa28dd4a86d1e2f158924469b97eed5f33d1
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-05-16 12:40:11 -0700

    fix #13 by caching the entry assembly

--- src/Microsoft.Extensions.PlatformAbstractions/ApplicationEnvironment.cs
+++ src/Microsoft.Extensions.PlatformAbstractions/ApplicationEnvironment.cs
@@ -10,26 +10,29 @@ namespace Microsoft.Extensions.PlatformAbstractions
 {
     public class ApplicationEnvironment
     {
+        private static readonly Lazy<Assembly> _entryAssembly = new Lazy<Assembly>(GetEntryAssembly);
+        private static readonly Lazy<FrameworkName> _runtimeFramework = new Lazy<FrameworkName>(GetRuntimeFramework);
+        private static readonly Lazy<string> _appBase = new Lazy<string>(GetApplicationBasePath);
+
         public string ApplicationBasePath { get; } = GetApplicationBasePath();
 
-        public string ApplicationName { get; } = GetEntryAssembly()?.GetName().Name;
+        public string ApplicationName { get; } = _entryAssembly.Value?.GetName().Name;
 
-        public string ApplicationVersion { get; } = GetEntryAssembly()?.GetName().Version.ToString();
+        public string ApplicationVersion { get; } = _entryAssembly.Value?.GetName().Version.ToString();
 
-        public FrameworkName RuntimeFramework
+        public FrameworkName RuntimeFramework => _runtimeFramework.Value;
+
+        private static FrameworkName GetRuntimeFramework()
         {
-            get
-            {
-                string frameworkName = null;
+            string frameworkName = null;
 #if NET451
-                // Try the setup information
-                frameworkName = AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
+            // Try the setup information
+            frameworkName = AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName;
 #endif
-                // Try the target framework attribute
-                frameworkName = frameworkName ?? GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
+            // Try the target framework attribute
+            frameworkName = frameworkName ?? GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
 
-                return string.IsNullOrEmpty(frameworkName) ? null : new FrameworkName(frameworkName);
-            }
+            return string.IsNullOrEmpty(frameworkName) ? null : new FrameworkName(frameworkName);
         }
 
         private static string GetApplicationBasePath()
@@ -49,11 +52,13 @@ namespace Microsoft.Extensions.PlatformAbstractions
 #if NET451
             return Assembly.GetEntryAssembly();
 #else
-            // TODO: Remove private reflection when we get this: https://github.com/dotnet/corefx/issues/4146
+            // We use reflection because the API exists on almost all .NET Standard versions but is hidden
+            // Technically, since it isn't in the standard, there may be a platform that does not support it,
+            // but in that case we gracefully fail, returning null
             var getEntryAssemblyMethod =
                 typeof(Assembly).GetMethod("GetEntryAssembly", BindingFlags.Static | BindingFlags.NonPublic) ??
                 typeof(Assembly).GetMethod("GetEntryAssembly", BindingFlags.Static | BindingFlags.Public);
-            return getEntryAssemblyMethod.Invoke(obj: null, parameters: Array.Empty<object>()) as Assembly;
+            return getEntryAssemblyMethod?.Invoke(obj: null, parameters: Array.Empty<object>()) as Assembly;
 #endif
         }
     }
